e613d0ad3662f3f217d9bdcfcb180b1bdcd81523
sb.kang <sb.kang@q-sol.co.kr>
2021-01-27 14:38:46 +0900

    #1 프로젝트 초기화 테스트

--- src/com/sbkang/personal/java8/PersonalJava8Application.java
+++ src/com/sbkang/personal/java8/PersonalJava8Application.java
@@ -3,6 +3,7 @@ package com.sbkang.personal.java8;
 public class PersonalJava8Application {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("test");
 	}
 
 }
